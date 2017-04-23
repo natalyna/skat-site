4bcf2f5c285d2380627fdd1e2f974d73493738e3
nnogieva <n.nogieva@dodopizza.com>
2017-04-23 12:25:49 +0300

    libs, header first steps

--- app/router.js
+++ app/router.js
@@ -7,6 +7,11 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('projects');
+  this.route('buy');
+  this.route('company');
+  this.route('news');
+  this.route('contacts');
 });
 
 export default Router;
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,12 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    sassOptions: {
+      includePaths: [
+        'node_modules/ember-flexboxgrid-sass/',
+        'node_modules/sass-flex-mixin/'
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
