49fc2b389919f7d6451424ccab924b5e1740e8f5
nnogieva <n.nogieva@dodopizza.com>
2017-04-29 12:03:52 +0300

    Projects on index

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	title: 'Главная'
+	title: 'Главная',
+	model() {
+		return Ember.RSVP.hash({
+			projects: this.store.findAll('project', { limit: 2 })
+				.then(function(result) {
+					return result.slice(0, 2);
+				}),
+			//companies: this.store.findAll('company')
+		});
+	}
 });
