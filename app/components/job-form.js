05a3df397157463f2e7a59172e5a20fb4aa5a269
nnogieva <n.nogieva@dodopizza.com>
2017-05-20 12:52:12 +0300

    Contacts job form

--- app/components/job-form.js
+++ app/components/job-form.js
@@ -1,5 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	classNames: ['job-form']
+	classNames: ['job-form'],
+
+	actions: {
+		inputChanged(inputName) {
+			var $input = this.$('[name=' + inputName + ']'),
+				val = $input.val();
+
+			if (typeof val !== "undefined" && val !== "") {
+				$input.addClass("animated-input__input_not-empty");
+			} else {
+				$input.removeClass("animated-input__input_not-empty");
+			}
+		}
+	}
 });
