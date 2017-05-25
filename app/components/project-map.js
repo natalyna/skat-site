bd6e8ddc6c8d8475c264e40e96052684a427f130
nnogieva <n.nogieva@dodopizza.com>
2017-05-25 21:51:47 +0300

    Map events added

--- app/components/project-map.js
+++ app/components/project-map.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	classNames: ["map__g-map"],
+
+	mouseLeave() {
+		var $overlay = this.$(".map__overlay");
+
+		if (!$overlay.is(":visible")) {
+			$overlay.show();
+		}
+	},
+
+	actions: {
+		enableMap() {
+			var $overlay = this.$(".map__overlay");
+			$overlay.hide();
+		}
+	}
 });
