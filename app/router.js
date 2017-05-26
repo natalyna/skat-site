cb45cfa73f17005827c25bfe4722ae74933c7a00
nnogieva <n.nogieva@dodopizza.com>
2017-05-26 19:27:01 +0300

    Three level routes

--- app/router.js
+++ app/router.js
@@ -2,33 +2,39 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-	location: config.locationType,
-	rootURL: config.rootURL
+  location: config.locationType,
+  rootURL: config.rootURL
 });
 
 Router.map(function() {
-	this.route('projects', function() {
-		this.route('show', { path: '/show/:project_id' });
-	});
-	this.route('buy', function() {
-		this.route('process');
-		this.route('mortgage');
-		this.route('trade-in');
-		this.route('capital');
-		this.route('compensation');
-	});
-	this.route('company', function() {
-		this.route('team');
-		this.route('history');
-		this.route('services');
-	});
-	this.route('news', function() {
-		this.route('show', { path: '/show/:news_id' });
-	});
-	this.route('contacts');
-	this.route('vacancies', function() {
-		this.route('show', { path: '/show/:vacancy_id' });
-	});
+  this.route('projects', function() {
+    this.route('show', function() {
+      this.route('index', { path: '/:project_id' });
+      this.route('tech', { path: '/tech/:project_id' });
+      this.route('location', { path: '/location/:project_id' });
+      this.route('yard', { path: '/yard/:project_id' });
+      this.route('mortgage', { path: '/mortgage/:project_id' });
+    });
+  });
+  this.route('buy', function() {
+    this.route('process');
+    this.route('mortgage');
+    this.route('trade-in');
+    this.route('capital');
+    this.route('compensation');
+  });
+  this.route('company', function() {
+    this.route('team');
+    this.route('history');
+    this.route('services');
+  });
+  this.route('news', function() {
+    this.route('show', { path: '/show/:news_id' });
+  });
+  this.route('contacts');
+  this.route('vacancies', function() {
+    this.route('show', { path: '/show/:vacancy_id' });
+  });
 });
 
 export default Router;
